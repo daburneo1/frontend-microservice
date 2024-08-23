94cf181cccea76c316df98c915954a570a3e7590
daburneo1 <davichov2013@gmail.com>
2024-08-22 21:00:43 -0500

    feat: Cambio de fecth a axios para las consultas

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -3,45 +3,75 @@ import axios from 'axios';
 const API = import.meta.env.VITE_NODE_GATEWAY;
 
 export async function fetchProducts(page, limit) {
-    const response = await axios.get(`${API}products`, {
-        params: { page, limit }
-    });
-    return response.data;
+    try {
+        const response = await axios.get(`${API}products`, {
+            params: { page, limit }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching products:', error);
+        throw error;
+    }
 }
 
 export async function fetchProductById(id) {
-    const response = await axios.get(`${API}products/${id}`);
-    return response.data;
+    try {
+        const response = await axios.get(`${API}products/${id}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching product by ID:', error);
+        throw error;
+    }
 }
 
 export async function updateProductById(id, productData) {
-    const response = await axios.patch(`${API}products/${id}`, productData, {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
-    return response.data;
+    try {
+        const response = await axios.patch(`${API}products/${id}`, productData, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error updating product by ID:', error);
+        throw error;
+    }
 }
 
 export async function deleteProductById(id) {
-    const response = await axios.delete(`${API}products/${id}`);
-    return response.data;
+    try {
+        const response = await axios.delete(`${API}products/${id}`);
+        return response.data;
+    } catch (error) {
+        console.error('Error deleting product by ID:', error);
+        throw error;
+    }
 }
 
 export async function cancelOrder(orderId) {
-    const response = await axios.patch(`${API}orders/${orderId}`, { status: 'CANCELLED' }, {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
-    return response.data;
+    try {
+        const response = await axios.patch(`${API}orders/${orderId}`, { status: 'CANCELLED' }, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error canceling order:', error);
+        throw error;
+    }
 }
 
 export async function addProduct(productData) {
-    const response = await axios.post(`${API}products`, productData, {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
-    return response.data;
+    try {
+        const response = await axios.post(`${API}products`, productData, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error adding product:', error);
+        throw error;
+    }
 }
